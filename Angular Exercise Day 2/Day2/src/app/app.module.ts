7a597052e0e46a514a2d672ce3bd264600fb37eb
Kimberley Blankenhorn <kvandlen@yahoo.com>
2020-10-20 14:43:35 +0200

    angular 2

---
+++ Angular Exercise Day 2/Day2/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ListComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Angular Exercise Day 2/Day2/src/app/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+	
+
+
+  countClick(){
+         
+ }
+
+ cars = [
+   {
+     type: "Audi",
+     price: 30000
+   },{
+     type: "Land Rover",
+     price: 50000
+   },{
+     type:"Mercedes",
+     price: 40000
+   },
+ ];
+
+   constructor() { }
+
+  ngOnInit() {
+  }
+
+}
